6b2842a99283bc2918e3862dae0ffb282699dd3d
jaesukdo1986 <aviando1986@gmail.com>
2019-11-12 22:50:31 +0700

    delete some rows

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,20 +1,3 @@
-resource "aws_vpc" "this" {
-  count = var.create_vpc ? 1 : 0
-
-  cidr_block                       = var.cidr
-  instance_tenancy                 = var.instance_tenancy
-  enable_dns_hostnames             = var.enable_dns_hostnames
-  enable_dns_support               = var.enable_dns_support
-  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
-
-  tags = merge(
-    {
-      "Name" = format("%s", var.name)
-    },
-    var.tags,
-    var.vpc_tags,
-  )
-}
 
 resource "aws_vpc_ipv4_cidr_block_association" "this" {
   count = var.create_vpc && length(var.secondary_cidr_blocks) > 0 ? length(var.secondary_cidr_blocks) : 0
